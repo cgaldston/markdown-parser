35f1bfc128cd54e60749aef4e1ae2dce33cf75ce
Caleb Galdston <56410571+cgaldston@users.noreply.github.com>
2022-04-15 11:13:05 -0700

    fixed image error

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,6 +16,10 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            if (markdown.charAt(openBracket - 1) == '!') {
+                currentIndex = closeParen + 1;
+                continue;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
